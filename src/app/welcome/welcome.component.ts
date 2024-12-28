066ccaf383b4c1c6cc9836cac32d444ba5592e0b
guiziis <guigamoda@hotmail.com>
2024-12-28 18:56:47 -0300

    migrate welcomeComponent to standAlone

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
   selector: 'app-welcome',
+  imports: [DetailsComponent],
   templateUrl: './welcome.component.html',
+  standalone: true
 })
 export class WelcomeComponent {}
