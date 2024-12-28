840b4f60ac13c517f2995b5ca72059e3c0328e84
guiziis <guigamoda@hotmail.com>
2024-12-28 19:12:26 -0300

    migrate service to main.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AnalyticsService } from './shared/analytics.service';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [WelcomeComponent]
+  imports: [WelcomeComponent],
 })
 export class AppComponent {}
